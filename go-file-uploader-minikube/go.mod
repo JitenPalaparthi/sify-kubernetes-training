1c7c1e69d41b672b68fae3bebb0a1fe6f665b937
Jiten <jpalaparthi@gmail.com>
2026-03-13 01:33:33 +0530

    remove old go application

--- go-file-uploader-minikube/go.mod
+++
@@ -1,3 +0,0 @@
-module github.com/example/go-file-uploader
-
-go 1.22
--- go-file-uploader-minikube/scripts/build-and-load.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-IMAGE_NAME="go-file-uploader:v1"
-
-echo "Building image directly inside minikube..."
-minikube image build -t "${IMAGE_NAME}" .
-
-echo "Done. Image available to the cluster as ${IMAGE_NAME}"
