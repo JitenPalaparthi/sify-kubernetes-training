dcae7af8e62ca0b9f03bd8e2f9a43fa175fa35e0
Jiten <jpalaparthi@gmail.com>
2026-03-21 00:20:00 +0530

    add files

--- sonarqube-go-k8s-ci-cd-fresh/scripts/deploy-sonarqube.sh
+++ sonarqube-go-k8s-ci-cd-fresh/scripts/deploy-sonarqube.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -euo pipefail
 kubectl apply -f k8s/sonarqube/00-namespace.yaml
+kubectl apply -f k8s/sonarqube/00a-postgres-pv.yaml
+kubectl apply -f k8s/sonarqube/00b-sonarqube-pv.yaml
 kubectl apply -f k8s/sonarqube/01-postgres-secret.yaml
 kubectl apply -f k8s/sonarqube/02-postgres-pvc.yaml
 kubectl apply -f k8s/sonarqube/03-postgres-deployment.yaml
